e5e8884b8e2314669559dd8d7259157ac2b7fc88
MinhajHameed <minhajham@yahoo.com>
2024-02-03 11:19:32 +0530

    fix: resovle issue

---
+++ client/src/components/GitBtn.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import github from "../assets/images/github.svg";
+
+
+const GitBtn = () => {
+  const [isShaking, setIsShaking] = useState(false);
+
+  useEffect(() => {
+    const startShaking = () => {
+      setIsShaking(true);
+      // After a short duration, stop shaking and reset background color to white
+      setTimeout(() => {
+        setIsShaking(false);
+      }, 500);
+    };
+
+    const intervalId = setInterval(startShaking, 4000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <button
+      type="button"
+      onClick={() => window.open('https://github.com/minhajHameed')}
+      className={`fixed bottom-8 right-14 z-10 py-2 px-2 rounded-lg  ${
+        isShaking ? 'bg-yellow-400 shake shadow-yellow' : 'bg-stone-50 text-white shadow-md shadow-cyan-50/50'
+      }`}
+    >
+      <img src={github} alt="github" className="w-8 bg-transparent object-contain" />
+    </button>
+  );
+}
+
+export default GitBtn;
+
---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractAddress = '0x4C374B3Df4547c6d37f28Ee4E358bf6A2E0E8D67';
+export const contractABI = abi.abi;
